d9aa12b96a6e1c20352fb26ef8d417c9106b4352
Haitec <me@mrhaitec.de>
2017-02-11 11:25:35 +0100

    Mapbox integration

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+declare const mapboxgl: any;
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
-export class HelloIonicPage {
-  constructor() {
+export class HelloIonicPage implements OnInit {
 
+  ngOnInit() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibDIxMTA1MjQiLCJhIjoiY2l6MTFjOTV3MDAxbzJ3a2E1MjJrdm4xYyJ9.fDGPLz7xwTULovMwYPheCg';
+    var map = new mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
+      center: [-74.50, 40], // starting position
+      zoom: 9 // starting zoom
+    });
   }
 }
