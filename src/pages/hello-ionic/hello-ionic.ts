6b1ce7c773a6e07608146e2d321121b212f8eddf
Haitec <me@mrhaitec.de>
2017-02-11 13:54:58 +0100

    leaflet integratino

--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-declare const mapboxgl: any;
+import * as mapboxgl from 'mapbox-gl';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'page-hello-ionic',
@@ -9,12 +10,19 @@ declare const mapboxgl: any;
 export class HelloIonicPage implements OnInit {
 
   ngOnInit() {
-    mapboxgl.accessToken = 'pk.eyJ1IjoibDIxMTA1MjQiLCJhIjoiY2l6MTFjOTV3MDAxbzJ3a2E1MjJrdm4xYyJ9.fDGPLz7xwTULovMwYPheCg';
-    var map = new mapboxgl.Map({
-      container: 'map', // container id
-      style: 'mapbox://styles/mapbox/streets-v9', //stylesheet location
-      center: [-74.50, 40], // starting position
-      zoom: 9 // starting zoom
-    });
+
+    // [11.074339, 49.445591]
+    // [49.445591, 11.074339]
+    var token = 'pk.eyJ1IjoibDIxMTA1MjQiLCJhIjoiY2l6MTFjOTV3MDAxbzJ3a2E1MjJrdm4xYyJ9.fDGPLz7xwTULovMwYPheCg';
+
+    var mymap = L.map('map').setView([49.445591, 11.074339], 13);
+    
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: token
+    }).addTo(mymap);
+
   }
 }
